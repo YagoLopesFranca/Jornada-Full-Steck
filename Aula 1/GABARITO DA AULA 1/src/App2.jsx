26f64b5f6df3b723d146090ffd7d2bc207159a73
Yago Lopes França Rosa <yagolfr1995@gmail.com>
2025-07-14 17:00:06 -0300

    finalizando

---
+++ Aula 1/GABARITO DA AULA 1/src/App2.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const App2 = () => {
+  return <div>App2</div>;
+};
+
+export default App2;
+
+// rafce
+
+// Arrow Function
+// const App2 = () => <h1>Olá, mundo!</h1>
+
+// Nomeação de componente
+// PascalCase
+
+// Nomeação de Variavel, Função...
+// camelCase
+
+// export default, posso importa com qualquer nome e sem chaves
+// export "sem default", só posso importar entre chaves e com o nome exportado
+
+// self closing tag
+// <Header></Header>
+// <Header/>
+
+// Nomeação de classe no Css
+// Metodologia BEM
+// Blocks  - componentes
+// Elements
+// Modifiers
+// bloco__elemento--modificador
+// header
+// header__link
+// header__link--small
+// item-list__header
+// Nomes compostos são separados por -
+
+// Tag vazia em React se chama Fragment ou fragmento
